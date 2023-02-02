4ae9267df96327f41062a20b2daa2c45790e7b2f
Kline Gareth <klinegareth@gmail.com>
2023-02-02 11:46:01 -0500

    Added narrative <3

---
+++ src/routes/networked-media/narrative/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import TextBox from '$lib/components/TextBox.svelte';
+	import ImageBox from '$lib/components/ImageBox.svelte';
+	import NavBox from '$lib/components/NavBox.svelte';
+    
+    import post from "$lib/content/networked-media/narrative.json"
+
+    
+</script>
+
+<svelte:head>
+	<title>vernacular web | networked media </title>
+	<link rel="icon" href="/favicon.ico" />
+</svelte:head>
+
+<div class="all">
+	<div class="left">
+		<div class="topleft">
+			<ImageBox image="angels.png" link="https://graylure.tumblr.com/post/700287252594573312"/>
+		</div>
+		<div class="bottomleft">
+			<NavBox />
+		</div>
+	</div>
+	<div class="right">
+			<TextBox title={post.title}>{@html post.content}</TextBox>
+	</div>
+</div>
+
+<style>
+</style>
